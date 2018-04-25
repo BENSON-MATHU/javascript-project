00e1d5834b9ee877bbd05d3b60457a1dfd376995
BENSON-MATHU <bensonmuchoki56@gmail.com>
2018-04-25 16:35:21 +0300

    Adding the ready method to tell js load after page completes and using the anonymous callback

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
+jQuery(document).ready(function(){
+  // tell js to check html doc after finishing to load the page.
+    jQuery("h1").click(function() {
     alert("This is a header.");
   });
+  // js selects the h1 and click method creats and event listener that reacts after the header is clicked
+  // after clicking the alert is called.
 
   jQuery("p").click(function() {
     alert("This is a paragraph.");
